be14df0d02fe193303f2f67bcec6ca34593c1dca
Nick Podratz <nick@podratz.de>
2017-06-10 12:18:43 +0200

    Initial Commit

---
+++ MensaplanDesktop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MensaplanDesktop
+//
+//  Created by Nick Podratz on 10.06.17.
+//  Copyright © 2017 Nick Podratz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
